e901d2e309128f755e6963131154e671141120b8
gojakobii <gojakobii@gmail.com>
2023-07-06 11:19:46 -0500

    dynamic routing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,17 @@ import React from "react";
 import FooterLogo from "../assets/FooterLogo";
 
 function Footer() {
+  const base_uri = "/FetchFind";
+
   return (
     <footer className="bg-footer-texture bg-center bg-no-repeat">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="my-6 md:flex md:justify-between">
           <div className="md:mb-0">
-            <a href="/find" className="flex items-center justify-center">
+            <a
+              href={`${base_uri}/find`}
+              className="flex items-center justify-center"
+            >
               <FooterLogo />
             </a>
           </div>
